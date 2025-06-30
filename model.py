145752c2b3ef322bc018bc5f0b5a51f203207316
Viktoria <viktorij054@gmail.com>
2025-06-30 12:09:47 +0300

    model.py

---
+++ model.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+def train_model():
+    df = pd.read_csv("data/fake_or_real_news.csv")
+    X = df['text']
+    y = df['label']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+    X_train_vec = vectorizer.fit_transform(X_train)
+    X_test_vec = vectorizer.transform(X_test)
+
+    model = LogisticRegression()
+    model.fit(X_train_vec, y_train)
+
+    y_pred = model.predict(X_test_vec)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    return model, vectorizer, accuracy
