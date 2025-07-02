f1cb8be079125a71c78682eb98dc383fa11bc8d5
Viktoria <viktorij054@gmail.com>
2025-07-02 16:08:34 +0300

    model.py

--- model.py
+++ model.py
@@ -5,11 +5,22 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 def train_model():
-    df = pd.read_csv("data/fake_or_real_news.csv")
-    X = df['text']
-    y = df['label']
+    real = pd.read_csv("data/real.csv")
+    real['label'] = 'REAL'
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    fake = pd.read_csv("data/fake.csv")
+    fake['label'] = 'FAKE'
+
+    df = pd.concat([real, fake], ignore_index=True)
+
+    df = df.dropna(subset=['text'])
+
+    X = df["text"]
+    y = df["label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
     vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
     X_train_vec = vectorizer.fit_transform(X_train)
